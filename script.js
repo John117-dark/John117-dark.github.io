5baa55c989ee0fd412f16558e189429a32f1129e
Juan Eduardo Salinas <vjdark2020@outlook.com>
2024-10-23 14:12:56 -0600

    Intento de algo
    algo

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const burger = document.querySelector('.burger');
+        const navLinks = document.querySelector('.nav-links');
+
+        burger.addEventListener('click', function() {
+            navLinks.classList.toggle('nav-active');
+        });
+    });
+</script>
